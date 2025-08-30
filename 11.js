7b2a9293745f5bb4f5bf92d8ecbe44715ca3e583
Dulas <abdullahtshabalala@gmail.com>
2025-08-30 13:41:06 +0200

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,195 @@
+// series.js
+const seriesData = [
+
+{
+    title: "Arcane",
+    image: "Images/A.jpg",
+    description: ".",
+    genres: ["Drama", "Action","Sci-Fi","Steampunk","Adventure"],
+    episodes: [
+                      { title: "Episode 1", link:"https://drive.google.com/file/d/1kjRAix1nSwnqaaLhTUo-Kq1dus6CwNbI/preview " },
+                      { title: "Episode 2", link: "https://drive.google.com/file/d/1kdjJlwn-0tDoNOsXjd2NIFqRR0dTE8nP/preview " },  
+                      { title: "Episode 3", link:"https://drive.google.com/file/d/1mnDSLVXGjmS-s1fgty_OWogtDflkVD4o/preview" },    
+
+    ]
+  },
+
+
+{
+    title:"Solo leveling (s2)",
+    image:"Images/sl.jpeg",
+    description:"Solo Leveling: Season 2 – Arise from the Shadow continues Sung Jin-woo’s ascent as the Shadow Monarch. With his new powers hidden from the world, he faces even deadlier foes—including escaped S-Rank demons—and races to prevent a looming catastrophe brought by a mysterious hunter’s warning. ",
+    genres: ["Fantasy", "Action","Anime","Adventure","Shonen" ],
+    episodes: [
+                      { title: "Episode 1",link:"https://drive.google.com/file/d/1lFQb-JsOexeb7bxImzDu_c6ph0sxKc02/preview " }, 
+                      { title: "Episode 2", link: "https://drive.google.com/file/d/1lo9Hcl4aQccxR1vX-wES76uYEsSoSbeW/preview " },
+{ title: "Episode 3", link: " https://drive.google.com/file/d/1m8hXrXPZiK2YnHbKmgRXrnC83zCCNXyJ/preview" },
+{ title: "Episode 4", link: " https://drive.google.com/file/d/1mCsRmaqfZn268wrJ2CiFVXzUJD8lSTIO/preview" },
+{ title: "Episode 5", link: " https://drive.google.com/file/d/1mNm_N_BxCnDQjbhPZqK-enVH__ANEJim/preview" },
+{ title: "Episode 6", link: " https://drive.google.com/file/d/1mb166q9gHj-I1du82m6QYFbtPbT5lccW/preview" },  
+                
+
+    ]
+  },
+
+
+{
+    title: "Attack On Titan(1)",
+    image: "Images/aot.jpeg",
+    description: "Attack on Titan: Season 1 follows Eren Yeager, his adopted sister Mikasa, and their friend Armin as humanity fights for survival against giant humanoid creatures known as Titans. After their hometown is destroyed, Eren vows to eradicate every Titan. Joining the military, they uncover dark secrets about the Titans, face brutal battles, and confront the limits of human courage and resilience. ",
+    genres: ["Adventure","Shonen","Mystery","Action", "Thriller", "Anime"],
+    episodes: [
+      { title: "Episode 1" ,link:" https://drive.google.com/file/d/1dVytq0QiSTu8Kb7FvKubT8FzwZ6wrsbd/preview" },
+      { title: "Episode 2", link: "https://drive.google.com/file/d/1dXMt9HRbYFBqXj9lEUDq4GI7BQDiEtn9/preview " },
+      { title: "Episode 3", link: "https://drive.google.com/file/d/1daZxMAOLS4yJQ9oTUGeqIEPzb9eirqzU/preview " },
+      { title: "Episode 4", link: "https://drive.google.com/file/d/1dbE2oinR8rKyz5U4K-dJ-0GybJnCVs_H/preview "},
+      { title: "Episode 5", link:"https://drive.google.com/file/d/1dl9thmq8A3yaZ04PVXjyyjo4FZqLhWQ8/preview "},
+
+
+    ]
+  },
+
+
+ {
+    title: "Wrong way to use healing magic",
+    image: "Images/magic.jpeg",
+    description: "Wrong Way to Use Healing Magic follows Kevin, a skilled healer who discovers that his magic works far differently than expected—healing others often comes with unpredictable and comedic consequences. As he navigates adventures, battles, and misunderstandings, Kevin learns to control his powers while facing both enemies and hilarious mishaps along the way. ",
+    genres: ["Adventure","Isekai","Comedy", "Anime", "Fantasy"],
+    episodes: [
+      { title: "Episode 1", link: "https://drive.google.com/file/d/1bGS7uEp-48wpOBOqD0SmOWHGmdJ4sRBO/preview " },
+      { title: "Episode 2", link: "https://drive.google.com/file/d/1bhyk4q8kRkJZtMATBEJzUL02ELYG6MKk/preview " },
+      { title: "Episode 3", link: "https://drive.google.com/file/d/1c0EDAR5iMhfSnz3iX_8-0hbEHa4tg-zu/preview " },
+      { title: "Episode 4", link: " https://drive.google.com/file/d/1dzxT0RDut56FnysMA1HX-tm9catC6lQ9/preview" },
+      { title: "Episode 5", link: "https://drive.google.com/file/d/1e-cXrZSX_GMx-2mj1tuuGBX9vl-mgA_j/preview " },
+
+
+    ]
+  },
+
+
+  {
+    title: "Highschool of the Dead",
+    image: "Images/hd.jpeg",
+    description: "Highschool of the Dead follows a group of high school students and their school nurse as they struggle to survive a sudden zombie apocalypse in Japan. Facing not only hordes of the undead but also human greed and fear, they must navigate danger, form alliances, and fight for survival while seeking safety and hope in a collapsing world. ",
+    genres: ["Anime", "Action","Horror","Ecchi" ,"Adventure"],
+    episodes: [
+                      { title: "Episode 1", link: "https://drive.google.com/file/d/1du5EZvUQ7UDoG_GKNb6DO1P8VpQZ7N7l/preview  " },
+                       { title: "Episode 2", link: "https://drive.google.com/file/d/1dv8qvFClTYJQB6oEBts0pveP7l1h5y1I/preview " },  
+                       { title: "Episode 3", link: "https://drive.google.com/file/d/1mcrT9drcz4m23tB5WMXbT_0uLV3Lhza8/preview " },
+{ title: "Episode 4", link: "https://drive.google.com/file/d/1mdo77A1fogb8AZs45Y4kEKBei4MMlbSo/preview " },
+{ title: "Episode 5", link: "https://drive.google.com/file/d/1mfe3GorN0aD08-IWFTtbk6icRdLB3JVK/preview " }, 
+
+    ]
+  },
+
+
+
+{
+    title:"Scorpion (s1)",
+    image:"Images/cover6.jpeg",
+    description:"A team of brilliant misfits led by tech genius Walter O'Brien. Recruited by Homeland Security, they tackle high-risk threats using their unique skills to solve complex problems. As they face life-or-death missions, the team also learns to navigate personal relationships and the challenges of working together.",
+    genres: ["Drama", "Action","Crime" ],
+    episodes: [
+                      { title: "Episode 1", link: "https://drive.google.com/file/d/1EJAx-1uee9V7MzE9M3hijPVFbvvNUfoN/preview " },                   
+                   
+    ]
+  },
+
+
+
+{
+    title: "Good Doctor (s1)",
+    image: "Images/cover5.jpeg",
+    description: "Dr. Shaun Murphy, a young surgeon with autism and savant syndrome, as he navigates hospital life, proving his medical skills while facing personal and professional challenges.",
+    genres: ["Drama", "Family", ],
+    episodes: [
+                      { title: "Episode 1", link: "https://drive.google.com/file/d/1DO8jLiL0mvJPTbteZuhkvcaeBwtjLA4I/preview " },
+                      { title: "Episode 2", link: " https://drive.google.com/file/d/1Dm3Cqt4P55e0oD7ERBDNZF73mkV9nLrM/preview" },
+                      { title: "Episode 3", link: "https://drive.google.com/file/d/1DoAejUM2Z3JgHj9wTbfVX-2x_g4pcBzA/preview " },
+    ]
+  },
+
+
+
+
+
+  {
+    title: "Bleach:TYBW (S2)",
+    image: "Images/cour2.jpeg",
+    description: "A battle for the soul of the universe continues.",
+    genres: ["Action", "Fantasy", "Anime","Shonen"],
+    episodes: [
+      { title: "Episode 1", link: "https://drive.google.com/file/d/10D0xtKf_CPz5ODJAiX2A5wfyCg57NcIg/preview" },
+      { title: "Episode 2", link: "https://drive.google.com/file/d/1-K6_2qaz1ZCz7u54FSnNX-hPq3qbqmZf/preview" },
+      { title: "Episode 3", link: "https://drive.google.com/file/d/1-TQNzstys6szmuu-5iPmnxmZ6-oznFc2/preview" },
+{
+ title: "Episode 4", link: "https://drive.google.com/file/d/19ln3Gofwt97mvsIod7x6X33UXXjYjxjb/preview"},
+
+{
+ title: "Episode 5", link: "https://drive.google.com/file/d/1A4cR5foWbEwlpz9QfXhPMvnwdAHqLIfM/preview"},
+
+
+    ]
+  },
+
+
+  {
+    title: "Konosuba (s1)",
+    image: "Images/Cover4.jpg",
+    description: "Konosuba: Season 1 follows Kazuma Satou, a shut-in gamer who dies in a freak accident and is reincarnated in a fantasy world. Tasked with defeating the Demon King, he forms a dysfunctional party with the goddess Aqua, the mage Megumin, and the crusader Darkness. Together, their misadventures and comical failures highlight the humorous side of fantasy quests.",
+    genres: ["Comedy", "Fantasy", "Isekai", "Anime","Adventure"],
+    episodes: [
+      { title: "Episode 1", link: "https://drive.google.com/file/d/110UyhRjYnSOomo3-9Wh30BZonjVHr7Nu/preview" },
+      { title: "Episode 2", link: "https://drive.google.com/file/d/1128cXGsLQ_wUcjls8hcZbfy-YbeRwChc/preview" },
+      { title: "Episode 3", link: "https://drive.google.com/file/d/110VFc93Wq49TvnsWq9AclCCsyt5scLSC/preview" },
+      { title: "Episode 4", link: "https://drive.google.com/file/d/119B_PFF3wtTyQMu3pu4-rBpN1rrfI7dR/preview" },
+      { title: "Episode 5", link: "https://drive.google.com/file/d/116FV6qv7Hjdpkj4v7deuoactP-ee-T9A/preview" },
+      { title: "Episode 6", link: "https://drive.google.com/file/d/11A96GHkRYHQ1uwRwa4eMiVBxbqM5YcmA/preview" },
+      { title: "Episode 7", link: "https://drive.google.com/file/d/11TiSpcFJr2N8r7GPyIGgmgy2tHfnQTtL/preview" },
+      { title: "Episode 8", link: "https://drive.google.com/file/d/11tAEoUZsUeQa4ja3-Nnt76nOKR2_w5wa/preview" },
+      { title: "Episode 9", link: "https://drive.google.com/file/d/11V8IleXjjcnlUAparkf44XBFEUwxK8Ou/preview" },
+      { title: "Episode 10", link: "https://drive.google.com/file/d/11uRZmLQxY52jeIsQKiw6vf2d1jTiCBlr/preview" },
+    ]
+  },
+  {
+    title: "Demon Slayer (s3)",
+    image: "Images/DM.jpg",
+    description: "A youth begins a quest to fight demons and save his sister after he finds his family slaughtered and his sister turned into a demon.",
+    genres: ["Anime","Adventure","Action", "Fantasy","Shonen", ],
+    episodes: [
+                      { title: "Episode 1", link: "https://drive.google.com/file/d/1XLQqntnaNZcYTaXIu_McK1m2mo_LZv56/preview " },
+                      { title: "Episode 2", link: " https://drive.google.com/file/d/1XmN5vFRFNesn-gMaEKP7J12UCmJLXd0s/preview" },
+                      
+    ]
+  },
+  {
+    title: "Wind Breaker (s1)",
+    image: "Images/wb.jpeg",
+    description: "Wind Breaker follows Haruka Sakura, a loner with a fiery temper and a love for fighting, as he transfers to Furin High School—a school notorious for its delinquent students. Contrary to his expectations, he discovers that the school's toughest students, known as Bofurin, are the town's protectors. As Haruka becomes entangled with Bofurin, he learns about their complex dynamics and faces challenges that test his strength and resolve.",
+    genres: ["Anime","Action","Shonen","Drama","Comedy" ],
+    episodes: [
+                      { title: "Episode 1", link: "https://drive.google.com/file/d/1az4hVHLA1vH_xrROD8VjiUPGEQESDm8Q/preview " },
+                  { title: "Episode 2", link: "https://drive.google.com/file/d/1b1RR4bk-IHYHCF-7DsinO-5HSdqpcgOv/preview " },   
+                     { title: "Episode 3", link: "https:drive.google.com/file/d/1cgQ2EGa8GZQNLQFHmH-NUlRkLztL8Rqj/preview " },
+{ title: "Episode 4", link: "https://drive.google.com/file/d/1cngB9Nx1TysiG9W1_mpSRDSNbr3DivUX/preview " },
+{ title: "Episode 5", link: "https://drive.google.com/file/d/1cszf6WptniPQQgm-imt6Y7nkSGJRDVvE/preview " },
+{ title: "Episode 6", link: "https://drive.google.com/file/d/1cwwVcMVjL0VM_llB0R-ToTFAF0LtR4Qn/preview " },
+{ title: "Episode 7", link: "https://drive.google.com/file/d/1d8c9U9vArqGV1ITvJ-7Tz2TL1FX0FUcL/preview " },
+{ title: "Episode 8", link: "https://drive.google.com/file/d/1dPNK8ZUwAGn_tVINxAcqU1mwoH63Ihlq/preview " },
+{ title: "Episode 9", link: "https://drive.google.com/file/d/1dOu_dLg4IM0IiclRsiK_vcQkMyNh2sXJ/preview " },
+{ title: "Episode 10", link: "https://drive.google.com/file/d/1dIqSmm6E55RfSKI8NoL6OHbZOM1bZN4F/preview "},
+{ title: "Episode 11", link: "https://drive.google.com/file/d/1dGl4ZDsYWMcXH5NtSHN9EU9aHVZNMyTb/preview "},
+{ title: "Episode 12", link: "https://drive.google.com/file/d/1d9BFwVuNt1RWSGDo8gmnO2_KiW3IH67z/preview " },
+{ title: "Episode 13", link: "https://drive.google.com/file/d/1d9fMQO6Db4SKvhJCaljOhyIjdt8ogF9i/preview " },
+                   
+    ]
+  },
+
+
+
+  
+
+
+
+
+];
