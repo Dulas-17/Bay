d4d42c1016bfc8a6cc65da23860f57764778dce9
Dulas <abdullahtshabalala@gmail.com>
2025-08-30 13:40:24 +0200

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,95 @@
+// movies.js
+const movieData = [
+
+{
+    title: "Maze Runner:The scorch trials",
+    image: "Images/st.jpeg",
+    description: "Maze Runner: The Scorch Trials continues Thomas and the Gladers’ journey after escaping the Maze. They face a harsh, desolate landscape known as the Scorch, full of dangerous terrain, hostile humans, and mysterious creatures. As they uncover the truth about the organization WCKD, Thomas must lead his friends through betrayal, survival, and impossible choices to protect those he cares about. ",
+    genres: ["Adventure", "Action","Thriller","Sci-Fi"],
+    link: "https://drive.google.com/file/d/1oM--0OMZxRbUOKGLoWvYTTycKsgKrnP0/preview "
+  },
+
+{
+    title: "Weathering with you",
+    image: "Images/wwy.jpeg",
+    description: "Weathering With You follows Hodaka, a runaway teen in Tokyo, who meets Hina, a girl with the mysterious ability to control the weather. As they grow close, Hina’s gift becomes both a blessing and a curse, forcing them to make difficult choices about love, sacrifice, and the balance between human desires and nature’s power. ",
+    genres: ["Adventure", "Romance","Fantasy","Slice of Life"],
+    link: "https://drive.google.com/file/d/1l_Rts-j3QIKjtPkv-I1AftrVheqJmRyk/preview "
+  },
+
+
+{
+    title: "Free Guy",
+    image: "Images/fg.webp",
+    description: "Free Guy follows Guy, a cheerful bank teller who discovers he is actually a background character in an open-world video game. As he becomes self-aware, Guy takes control of his destiny, teaming up with a real-world player to save the game from being shut down. Along the way, he learns what it means to be a true hero in both the digital and real worlds. ",
+    genres: [,"Adventure", "Comedy","Action","Fantasy"],
+    link: "https://drive.google.com/file/d/1cRVyAhKAWqFxwnUW21mJrjIWkqWeh0hk/preview"
+  },
+
+
+
+  {
+    title: "The Maze Runner",
+    image: "Images/cover7 .jpeg",
+    description: "Thomas loses his memory and finds himself trapped in a massive maze called the Glade. He and his friends try to escape from the maze and eventually learn that they are subjects of an experiment. ",
+    genres: ["Adventure", "Action", "Thriller", "Mystery", "Sci-Fi"],
+    link: "https://drive.google.com/file/d/1EDZUjrtc-fCUeaLyEx-Bes_wchTs-JAV/preview"
+  }, 
+
+  {
+    title: "Your Name",
+    image: "Images/Cover3.jpeg",
+    description: "Your Name tells the story of two teenagers, Mitsuha and Taki, who mysteriously begin swapping bodies despite living in different places. As they navigate each other’s lives, they form a deep connection that transcends time and distance. When a looming disaster threatens Mitsuha’s town, the two race against fate to save each other and uncover the truth behind their strange bond.",
+    genres: ["Animation", "Slice of Life","Romance", "Fantasy", "Drama"],
+    link: "https://drive.google.com/file/d/1apFvCPgIKV4t3y27dlzwCUqqMRI8UjJz/preview"
+  },
+
+  {
+    title: "The Life List",
+    image: "Images/Cover1.jpeg",
+    description: "The Life List follows Alex Rose, who, after her mother’s death, must complete a set of childhood aspirations she wrote at age 13 to inherit her estate. Guided by her mother’s sentimental video messages and aided by the estate’s executor, Brad, Alex embarks on a transformative year of stand-up comedy, family reunions, romantic missteps, and rediscovery—ultimately learning what truly matters and finding unexpected love. ",
+    genres: ["Drama", "Family", "Adventure"],
+    link: "https://drive.google.com/file/d/12tRqoJ9sXycE7RODnlxu11HVR7wcZbtL/preview"
+  },
+
+{
+    title: "Konosuba",
+    image: "Images/k.jpeg",
+    description: "Kazuma and his friends set out to help the village of the Crimson demons when they're attacked by the frightening demon king's army. ",
+    genres: ["Adventure", "Animation","Comedy","Fantasy","Isekai"],
+    link: "https://drive.google.com/file/d/1lgfFpM0JpatalyaOSyyHflhzUD0JwbgI/preview "
+  },
+
+
+  {
+    title: "Power Rangers",
+    image: "Images/Cover2.jpeg",
+    description: "Power Rangers (2017) follows five ordinary teenagers who discover ancient Power Coins that grant them extraordinary abilities. Guided by the mentor Zordon, they must learn to work together and embrace their roles as the Power Rangers to stop the villainous Rita Repulsa from destroying the world. ",
+    genres: ["Action", "Sci-Fi", "Adventure"],
+    link: "https://drive.google.com/file/d/17looZTle9cX9De1f6gN5QUcBhopDoKvx/preview"
+  },
+
+  {
+    title: "Fight or Flight",
+    image: "Images/fof.jpeg",
+    description: "A mercenary takes on the job of tracking a high-value asset known only as \"The Ghost\" on an international flight. Realizing the plane is filled with assassins assigned to kill them both, the pair must work together in a fight for their lives.",
+    genres: ["Action", "Thriller"],
+    link: "https://drive.google.com/file/d/1NOmVKcv5iDy3cEDIvgTb9j9801lQTQc1/preview"
+  },
+
+  {
+    title: "The Swimmers",
+    image: "Images/ts.jpeg",
+    description: "Two Syrian sisters flee their war-torn home in Damascus, swim for hours in choppy Mediterranean seas to reach Greece as asylum seekers before going on to compete at the Rio Olympic Games.",
+    genres: ["Adventure", "Bibliography", "Drama"],
+    link: "https://drive.google.com/file/d/1JkUj0q7_zGIiHegH40aDmYQW8wxmW5mR/preview"
+  },
+
+  {
+    title: "The Kissing Booth",
+    image: "Images/tkb.jpeg",
+    description: "A high school student finds herself face-to-face with her long-term crush when she signs up to run a kissing booth at the spring carnival.",
+    genres: ["Romance", "Comedy","Drama"],
+    link: "https://drive.google.com/file/d/1KeGGJbvLWWEXeZH_CAOotK75Wdi-yR7C/preview"
+  },
+];
